fdc26d57844f2cb1512c7b5685e3fa0d84fcedb8
robbie29 <37903975+robert-manolache@users.noreply.github.com>
2021-10-16 22:36:39 +1100

    expand image subplots to include efficient-net class preds

--- pawpy/data_viz.py
+++ pawpy/data_viz.py
@@ -5,21 +5,35 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras.applications.efficientnet import decode_predictions
 
-def img_gen_subplots(img_gen, n_runs=5):
+def img_gen_subplots(img_gen, n_runs=5, model=None, model_type="eff_net"):
+    """
+    Later we'll pass predictions from own models to compare with actual scores!
+    """
     
-    plt.figure(figsize=(img_gen.batch_size*2.5, n_runs*2.5))
+    plt.figure(figsize=(img_gen.batch_size*4, n_runs*4))
     a = 0
     
     for r in range(n_runs):
     
         x = img_gen.next()
+        if model is not None:
+            if model_type == "eff_net":
+                preds = decode_predictions(model.predict(x))
         
         for i in range(x[0].shape[0]):
 
             a += 1
+            if model is not None:
+                if model_type == "eff_net":
+                    class_id, name, prob = preds[i][0]
             
             plt.subplot(n_runs, img_gen.batch_size, a)
             plt.imshow(x[0][i, :, :, :].astype(np.uint8))
             plt.axis("off")
-            plt.title("Score: %d"%x[1][i])
+            if model is None:
+                plt.title("Score: %d"%x[1][i])
+            else:
+                if model_type == "eff_net":
+                    plt.title("%s (%.1f%%) - %d"%(name, prob*100, x[1][i]))
