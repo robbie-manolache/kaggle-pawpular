c3e0132756bf7fad3c7c93881e7f2a9ade40a8ca
robbie29 <37903975+robert-manolache@users.noreply.github.com>
2021-11-23 21:30:01 +1100

    add script to download kernel outputs from kaggle

---
+++ __get_kernel_data__.py
@@ -0,0 +1,21 @@
+
+import os
+from pawpy import env_config
+from lazykaggler import kernel_output_download
+from datetime import datetime
+
+# set config and data directory
+env_config("config.json")
+user = "slashie"
+kernels = ["ppc-model-dev-pipe-0%d"%i for i in [1,2,3,4,5]]
+local_dir = os.path.join(os.environ.get("DATA_DIR"), 
+                         "kaggle-kernel-download")
+
+# run download
+for kernel in kernels:
+    now = datetime.now().strftime("_%Y%m%d_%H%M%S")
+    local_path = os.path.join(local_dir, kernel+now)
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+    kernel_output_download(user, kernel, local_path)
+    
